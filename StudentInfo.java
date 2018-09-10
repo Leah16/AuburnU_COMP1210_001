e093ed0b6d4bc5fbdb101cf02af190b038b41ebb
Leah16 <43130160+Leah16@users.noreply.github.com>
2018-09-10 01:57:53 -0500

    Add files via upload

---
+++ StudentInfo.java
@@ -0,0 +1,21 @@
+/**
+* Program to print my infomation.
+*
+* Activity 1
+* @author leah16 COMP1210-001
+* @version 2018/8/27 1.0
+*/
+
+public class StudentInfo
+{
+  /**
+   * Prints my infomation.
+   * @param args Command line arguments ¨C not used.
+   */
+   public static void main(String[] args)
+   {
+      System.out.println("Name: leah16");
+      System.out.println("Previous Computer Courses:");
+      System.out.println("   None");   
+   }
+}
